11d2e1238cf28b13009dbd734d95781c56037caf
John Lowe <jblowe@berkeley.edu>
2015-09-08 20:45:15 -0700

    NOJIRA: deploy the header logo as part of deployment. obviously.

--- setup.sh
+++ setup.sh
@@ -16,6 +16,8 @@ elif [ "$COMMAND" = "deploy" ]; then
     cp ~/django_example_config/$2 config
     cp config/main.cfg cspace_django_site
     rm config/*.json
+    cd cspace_django_site/static/cspace_django_site/images
+    cp header-logo-$2.png header-logo.png
 elif [ "$COMMAND" = "configure" ]; then
     cp cspace_django_site/extra_$2.py cspace_django_site/extra_settings.py
     cp cspace_django_site/all_urls.py cspace_django_site/urls.py
