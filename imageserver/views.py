647e7c46503b8c3d9c75b4b3da89124073067e02
John Lowe <jblowe@berkeley.edu>
2015-11-03 12:32:28 -0800

    BAMPFA-466: allow restrictions on images returned by imageserver; should be transparent to existing users

--- imageserver/views.py
+++ imageserver/views.py
@@ -3,7 +3,7 @@ __author__ = 'jblowe'
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
-from common import cspace # we use the config file reading function
+from common import cspace  # we use the config file reading function
 from cspace_django_site import settings
 
 from os import path
@@ -21,8 +21,13 @@ protocol = config.get('connect', 'protocol')
 port = config.get('connect', 'port')
 port = ':%s' % port if port else ''
 
-
-imageunavailable = config.get('connect', 'imageunavailable')
+imageunavailable = config.get('info', 'imageunavailable')
+try:
+    derivatives_served = config.get('info', 'derivatives_served')
+    derivatives_served = derivatives_served.split(',')
+except:
+    print 'No derivatives are restricted'
+    derivatives_served = None
 
 server = protocol + "://" + hostname + port
 
@@ -30,10 +35,25 @@ server = protocol + "://" + hostname + port
 logger = logging.getLogger(__name__)
 logger.info('%s :: %s :: %s' % ('imageserver startup', '-', '%s' % server))
 
-#@login_required()
+# @login_required()
 def get_image(request, image):
     try:
         elapsedtime = time.time()
+        # if the use is authenticated, they can see anything.
+        # otherwise, they see only what the imageserver is configured to let them see.
+        if not request.user.is_authenticated():
+            # if no list of authorized derivatievs is set in the config file, all are available
+            image_ok = False if derivatives_served else True
+            for derivative in derivatives_served:
+                if derivative in image:
+                    image_ok = True
+                    break
+            if not image_ok:
+                html = '''
+            <div style="height: 90px; width: 96px; background-color: lightgray; font-size: 80%;">
+            <br/>&nbsp;[not authorized]
+            </div>'''
+                return HttpResponse(html, content_type='text/html')
 
         passman = urllib2.HTTPPasswordMgr()
         passman.add_password(realm, server, username, password)
@@ -52,7 +72,7 @@ def get_image(request, image):
         elapsedtime = time.time() - elapsedtime
     except:
         logger.info('%s :: %s :: %s' % ('image error', '-', '%s :: %8.3f seconds' % (image, elapsedtime)))
-        image404 = open(path.join(settings.BASE_PARENT_DIR, 'cspace_django_site/static/cspace_django_site/images', imageunavailable),'r').read()
+        image404 = open(path.join(settings.BASE_PARENT_DIR, 'cspace_django_site/static/cspace_django_site/images', imageunavailable), 'r').read()
         return HttpResponse(image404, content_type='image/jpeg')
 
     logger.info('%s :: %s :: %s' % ('image', '-', '%s :: %8.3f seconds' % (image, elapsedtime)))
