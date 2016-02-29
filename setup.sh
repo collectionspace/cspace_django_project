972df01410e1896c79d26d12f078474d5fae6f49
John Lowe <jblowe@berkeley.edu>
2016-02-28 18:17:49 -0800

    CSPACE-6915: revise the setup script to work well with the demo config

--- setup.sh
+++ setup.sh
@@ -16,7 +16,13 @@ elif [ "$COMMAND" = "deploy" ]; then
     rm config/*.cfg
     rm config/*.csv
     rm config/*.xml
-    cp ~/django_example_config/$2/* config
+    if [ "$2" = "default" ]; then
+        cp config.examples/* config
+        cp cspace_django_site/static/cspace_django_site/images/CollectionToolzSmall.png cspace_django_site/static/cspace_django_site/images/header-logo.png
+    else
+        cp ~/django_example_config/$2/* config
+        cp cspace_django_site/static/cspace_django_site/images/header-logo-$2.png cspace_django_site/static/cspace_django_site/images/header-logo.png
+    fi
     mv config/main.cfg cspace_django_site
     rm fixtures/*.json
     mv config/*.json fixtures
@@ -25,14 +31,21 @@ elif [ "$COMMAND" = "deploy" ]; then
     python manage.py syncdb --noinput
     python manage.py migrate
     python manage.py loaddata fixtures/*.json
-    cp cspace_django_site/static/cspace_django_site/images/header-logo-$2.png cspace_django_site/static/cspace_django_site/images/header-logo.png
     python manage.py collectstatic --noinput
-    echo "Don't forget to configure cspace_django_site/main.cfg and the rest of the configuration files in config/"
+    echo
+    echo "*************************************************************************************************"
+    echo "Don't forget to modify cspace_django_site/main.cfg if necessary and check on the rest of the"
+    echo "configuration files in config/ (these are .cfg and .csv files)"
+    echo "*************************************************************************************************"
+    echo
 elif [ "$COMMAND" = "configure" ]; then
     cp cspace_django_site/extra_$2.py cspace_django_site/extra_settings.py
     cp cspace_django_site/all_urls.py cspace_django_site/urls.py
-    rm db.sqlite3
-    python manage.py syncdb --noinput
-    python manage.py migrate
-    python manage.py collectstatic --noinput
+    echo
+    echo "*************************************************************************************************"
+    echo "OK, \"$2\" is configured. Now run ./setup.sh deploy <tenant> to set up a particular tenant,"
+    echo "where <tenant> is either "default" (for non-specific tenant, i.e. nightly.collectionspace.org) or"
+    echo "an existing tenant in the django_example_config repo"
+    echo "*************************************************************************************************"
+    echo
 fi
